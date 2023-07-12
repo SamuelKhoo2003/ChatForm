7a7c424138d169b2b59b72c96c017c6c72e20c9c
Samuel Khoo <115562183+SamuelKhoo2003@users.noreply.github.com>
2023-07-12 14:36:04 +0800

    mod

--- base/views.py
+++ base/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request): 
-    return HttpResponse('Home page')
+    return render(request, 'home.html')
 
 def room(request): 
-    return HttpResponse('ROOM')
+    return render(request, 'room.html')
