7757b2d485880e3093e9e4e2263b5e41c21418bf
Samuel Khoo <115562183+SamuelKhoo2003@users.noreply.github.com>
2023-07-12 15:55:17 +0800

    templates v1

--- base/views.py
+++ base/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+rooms = [
+    {'id':1, 'name':'What is your favourite movie?'},
+    {'id':2, 'name':'Why should I learn to drive?'},
+    {'id':3, 'name':'Is it worth it to invest in Nvidia?'},
+]
+
 def home(request): 
-    return render(request, 'homepg.html')
+    context = {'rooms':rooms}
+    return render(request, 'base/homepg.html', context)
 
 def room(request): 
-    return render(request, 'roompg.html')
+    return render(request, 'base/roompg.html')
