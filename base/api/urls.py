dcfbb4eed2371708643e5b4343a00d61afd88232
Samuel Khoo <115562183+SamuelKhoo2003@users.noreply.github.com>
2023-07-24 09:06:34 +0800

    setup api using Django REST framework

--- base/api/urls.py
+++ base/api/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.getroutes),
-]
+    path('api/rooms', views.getrooms), 
+]   
--- base/api/views.py
+++ base/api/views.py
@@ -1,13 +1,25 @@
-from django.http import JsonResponse
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from base.models import Room
+from .serializers import RoomSerializer
 
 # this function shows us all the routes within our API
+@api_view(['GET'])
 def getroutes(request):
     routes = [
         'GET /api',
         'GET /api/rooms', 
         'GET /api/rooms/:id',
     ]
-    return JsonResponse(routes, safe=False)
+    return Response(routes)
 
 # we set safe = False as we want to be able to use more than the python dictionary here 
+# safe is only needed for JsonReponse 
+@api_view(['GET'])
+def getrooms(request): 
+    rooms = Room.objects.all()
+    serializer = RoomSerializer(rooms, many=True)
+    # we set many as true as we are serializing a query set which has multiple objects/pieces of data 
+    return Response(rooms)
+
+# we can configure the django-cors-headers to configure which urls will have accss to the api data and etc (this is not done in this instance)
